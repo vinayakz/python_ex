68b912689b8e6e1c37ac7978ee33a18c6c802a99
Vinayak Zutti <33689324+vinayakz@users.noreply.github.com>
2020-05-27 14:34:37 +0530

    add via gitBash

---
+++ alpha/numpy.py
@@ -0,0 +1,6 @@
+import numpy as np
+a = np.array([1,2,3,4])               # creates a 1-dimensional array
+b = np.array([[1,2,3,4], [5,6,7,8]])    # creates a 2-dimensional array
+print(a)
+print('----')
+print(b)
