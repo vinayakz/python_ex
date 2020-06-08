2ced10a76afc9adcc9770fa0ae3373984a03a8aa
Vinayak Zutti <33689324+vinayakz@users.noreply.github.com>
2020-06-08 12:40:41 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-search = "weather in India"
+search = input("Plz type weather in and city or state Name: ")
 url = f"https://www.google.com/search?&q={search}"
 r = requests.get(url)
 s = BeautifulSoup(r.text, "html.parser")
 update = s.find("div", class_="BNeawe").text
-print("Todays Weather In India :-",update)
+print("Weather In Your city or State :",update)
